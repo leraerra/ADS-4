a353259c4addd7b2838eec0b1a03a1e93c92e74a
leraerra <valeriaberbasova@yandex.ru>
2026-05-21 18:55:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,5 @@
 // Copyright 2021 NNTU-CS
 #include "alg.h"
-
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
@@ -19,9 +18,7 @@ int countPairs2(int *arr, int len, int value) {
         int need = value - arr[i];
         for (int j = len - 1; j > i; j--) {
             if (arr[j] < need) break;
-            if (arr[j] == need) {
-                count++;
-            }
+            if (arr[j] == need) count++;
         }
     }
     return count;
@@ -29,29 +26,20 @@ int countPairs2(int *arr, int len, int value) {
 
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
-    int left = 0;
-    int right = len - 1;
+    int left = 0, right = len - 1;
     while (left < right) {
         int sum = arr[left] + arr[right];
         if (sum == value) {
             if (arr[left] == arr[right]) {
                 int n = right - left + 1;
-                count += n * (n - 1) / 2;
-                break;
+                return count + n * (n - 1) / 2;
             }
-            int leftVal = arr[left];
-            int leftCount = 0;
-            while (left <= right && arr[left] == leftVal) {
-                leftCount++;
-                left++;
-            }
-            int rightVal = arr[right];
-            int rightCount = 0;
-            while (left <= right && arr[right] == rightVal) {
-                rightCount++;
-                right--;
-            }
-            count += leftCount * rightCount;
+            int lc = 1, rc = 1;
+            while (left + lc <= right && arr[left + lc] == arr[left]) lc++;
+            while (right - rc >= left && arr[right - rc] == arr[right]) rc++;
+            count += lc * rc;
+            left += lc;
+            right -= rc;
         } else if (sum < value) {
             left++;
         } else {
